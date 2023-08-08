09e494226f8cf0d87ff80ee3f75e8f894f72b5e0
Liuperson306 <129485929+Liuperson306@users.noreply.github.com>
2023-08-08 11:47:33 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 import streamlit as st
 import imaplib
+import time
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+from email.mime.application import MIMEApplication
 
 
 # 配置IMAP服务
@@ -40,15 +44,48 @@ def parse_email(raw_email):
     body = '\n'.join(lines[2:])
     return {'sender': sender, 'subject': subject, 'body': body}
 
+def send_email(email, password)
+
+    # 构建邮件主体
+    msg = MIMEMultipart()
+    msg['From'] = email
+    msg['To'] = email  # 收件人邮箱
+    msg['Subject'] = 'hello_' + localtime
+    
+    # 邮件正文
+    text = MIMEText('你好')
+    msg.attach(text)
+    
+    # # 添加附件
+    # with open(file_name, 'rb') as f:
+    #     attachment = MIMEApplication(f.read())
+    #     attachment.add_header('Content-Disposition', 'attachment', filename=file_name)
+    #     msg.attach(attachment)
+    
+    # 发送邮件
+    try:
+        smtp = smtplib.SMTP('smtp.163.com')
+        smtp.login(email, password)
+        smtp.sendmail(email, email, msg.as_string())
+        smtp.quit()
+        print('邮件发送成功')
+    except smtplib.SMTPException as e:
+        print('邮件发送失败，错误信息：', e)
+
+
+
 # Streamlit应用界面
-st.title('邮箱邮件访问应用')
-label = st.selectbox('选择邮件标签', ['INBOX', 'Sent', 'Spam', 'Custom Label'])
-keyword = st.text_input('搜索关键字')
-if st.button('读取邮件'):
-    emails = read_emails(label, keyword)
-    for email in emails:
-        st.write(f'发件人：{email["sender"]}')
-        st.write(f'主题：{email["subject"]}')
-        st.write(f'内容：{email["body"]}')
+st.title('邮箱邮件发送应用')
+if st.button('发送邮件'):
+    send_email(email, password)
+    
+# label = st.selectbox('选择邮件标签', ['INBOX', 'Sent', 'Spam', 'Custom Label'])
+# keyword = st.text_input('搜索关键字')
+# if st.button('读取邮件'):
+#     emails = read_emails(label, keyword)
+#     for email in emails:
+#         st.write(f'发件人：{email["sender"]}')
+#         st.write(f'主题：{email["subject"]}')
+#         st.write(f'内容：{email["body"]}')
 
 
