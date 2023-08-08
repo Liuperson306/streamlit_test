c723f66df3d4f9b549a5ebb4f1fc2f2ff3930591
Liuperson306 <129485929+Liuperson306@users.noreply.github.com>
2023-08-08 11:28:08 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import streamlit as st
 
 # 使用st.secrets读取敏感信息
+email = st.secrets["my_email"]["email"]
 email_password = st.secrets["my_email"]["password"]
-api_key = st.secrets["api_keys"]["my_api_key"]
 
 # 在应用中使用读取的信息
+st.write(f"API密钥：{email}")
 st.write(f"邮箱密码：{email_password}")
-st.write(f"API密钥：{api_key}")
+
