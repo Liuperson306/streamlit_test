8a47d5857271e51ac6c75a29e1e7f8f9f355e710
Liuperson306 <129485929+Liuperson306@users.noreply.github.com>
2023-08-18 15:07:42 +0800

    Update main.py

--- main.py
+++ main.py
@@ -66,7 +66,8 @@ def read_email(myemail, password):
         mail.logout()
         return array
     except Exception as e:
-        st.error(f'出现错误：{e}')
+        #st.error(f'出现错误：{e}')
+        st.error('网络问题，请刷新页面')
 
 def instrunction():
     st.subheader("Instructions: ")
@@ -95,8 +96,8 @@ def QA(data_face, data_lip, num):
     ans2 = get_ans(answer_2)
 
     # 保存结果到列表
-    data_face[num - 1] = ans1
-    data_lip[num - 1] = ans2
+    data_face[num-1] = ans1
+    data_lip[num-1] = ans2
 
 # 将用户的答案转化为1/0
 def get_ans(answer_str):
@@ -105,12 +106,17 @@ def get_ans(answer_str):
     elif "Right" in answer_str:
         return "0"
     
-# 播放Video文件下的视频
-def play_video(file_name,num):
-    st.subheader(fr"Video{num+1}/12")
-    # st.subheader(file_name)
-    st.video(fr'video/{file_name}',start_time=0)
-    st.write("Please answer the following questions, after you watch the video. ")
+# # 播放Video文件下的视频
+# def play_video(file_name,num):
+#     st.subheader(fr"Video{num+1}/12")
+#     #st.subheader(file_name)
+#     st.video(fr'video/{file_name}',start_time=0)
+#     st.write("Please answer the following questions, after you watch the video. ")
+
+@st.cache_data
+def play_video(file_name):
+    video_bytes = open(file_name, 'rb').read()
+    return video_bytes
 
 def data_collection(email, password, data_face, data_lip, random_num):
     # 发送内容
@@ -161,33 +167,52 @@ def page(random_num):
 
     if "button_clicked" not in st.session_state:
         st.session_state.button_clicked = False
+        
+    # Ours vs. VOCA 1-30
+    # Ours vs. MeshTalk 31-60
+    # Ours vs. FaceFormer 61-90
+    # Ours vs. GT 91-120
+    # title_num = 1
+    # for cate in range(4):
+    #     for num in range(3):
+    #         # 显示页面内容
+    #         st.write(f'这是第{num+1+random_num*3+cate*30}个视频，名称为{file_list[num+random_num*3+cate*30].rstrip()}')
+    #         play_video(file_list[num+random_num*3+cate*30].rstrip(),num)
+    #         st.write(f'这是第{title_num}题')
+    #         QA(data_face, data_lip, title_num)
+    #         title_num+=1
 
     for num in range(12):
         # 显示页面内容
-        st.write(f'这是第{num+1+random_num*12}个视频，名称为{file_list[num+random_num*12].rstrip()}')
-        play_video(file_list[num+random_num*12].rstrip(),num)
-        st.write(f'这是第{num+1}题')
+        #st.write(f'这是第{num+1+random_num*12}个视频，名称为{file_list[num+random_num*12].rstrip()}')
+        st.subheader(fr"Video {num+1}")
+        video_bytes = play_video('video/' + file_list[num+random_num*12].rstrip())
+        st.video(video_bytes)
+
+        st.write("Please answer the following questions, after you watch the video. ")
+        #st.write(f'这是第{num+1}题')
         QA(data_face, data_lip, num+1)
 
     if not st.session_state.button_clicked:
         if st.button("Submit results"):
-            st.write(random_num)
+            #st.write(random_num)
             array[random_num]+=1
             send_email(myemail, password, array)
-            st.write(array)
+            #st.write(array)
             data_collection(myemail, password, data_face, data_lip, random_num)
             st.session_state.button_clicked = True # 按钮设置为True
     if st.session_state.button_clicked == True:
-        st.balloons()
+        #st.balloons()
         st.success("Successfully submitted the results. Thank you for using it. Now you can exit the system.")
-        st.write(data_face,data_lip)
-
+        #st.write(data_face,data_lip)
 
 if __name__ == '__main__':
     myemail = st.secrets["my_email"]["email"]  # 邮箱账号
     password =  st.secrets["my_email"]["password"]  # 邮箱密码
     array = read_email(myemail, password)
-    
+    if all(element == 3 for element in array):
+        array = [0] * 10
+
     if "data_face" and "data_lip" not in st.session_state:
     # 初始化data变量
         data_face = [1 for x in range(0, 12)]
@@ -196,8 +221,6 @@ if __name__ == '__main__':
         # 恢复data变量的状态
         data_face = st.session_state["data_face"]
         data_lip = st.session_state["data_lip"]
-
-    st.write(array)
     random_num = 0
     # array = [0,0,0,0,0,0,0,0,0,0]
     if 'random_num' not in st.session_state:
@@ -209,8 +232,5 @@ if __name__ == '__main__':
                     break
 
     random_num = st.session_state.random_num
-    st.write(f'这是第{random_num+1}份试卷')
+    #st.write(f'这是第{random_num+1}份试卷')
     page(random_num)
-
-
-    
